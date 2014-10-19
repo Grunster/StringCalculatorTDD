e21c3d648566fea56321ba4f700494429536594d
Sigrun Thorsteins <sigrunt13@ru.is>
2014-10-19 22:15:24 +0000

    negatives leyst

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -4,7 +4,7 @@ import java.util.regex.*;
 
 public class Calculator {
 
-	public static int add(String text){
+	public static int add(String text) throws Calculator.NegativeException{
 		if(text.equals("")){
 			return 0;
 		}
@@ -18,7 +18,7 @@ public class Calculator {
 		return Integer.parseInt(number);
 	}
 
-	private static String[] splitNumbers(String numbers){
+	private static String[] splitNumbers(String numbers) throws Calculator.NegativeException{
 		if(numbers.contains("//")){
 		Pattern pattern = Pattern.compile("//(.)\n(.+)");
 		Matcher match = pattern.matcher(numbers);
@@ -26,28 +26,40 @@ public class Calculator {
 			if(match.matches()){
 				String delimeter = match.group(1);
 				String newNumbers = match.group(2);
-			return newNumbers.split(",|\n|" + delimeter);
+				return newNumbers.split(",|\n|" + delimeter);
 			}
 		}
 		return numbers.split(",|\n");
 	}
 
-   	private static int sum(String[] numbers){
+   	private static int sum(String[] numbers) throws Calculator.NegativeException{
  	   	int total = 0;
-       		for(String number : numbers){
-		    total += toInt(number);
+       		StringBuffer negative = new StringBuffer();
+		boolean isNegative = false;
+		for(String number : numbers){
+			if(number.contains("-")){
+				negative.append(number + ",");
+				isNegative = true;
+			}
+			else{
+				total += toInt(number);
+			}
 		}
-		return total;
+		if(isNegative){
+			String finished = negative.toString();
+			throw new NegativeException("Negatives not allowed: " + finished.substring(0, finished.length() - 1));
+		}
+		else{
+			return total;
+		}
     	}
 
-//	public static class NegativeException extends Exception{
-//		public NegativeException(){
-//			super();
-//		}
-//		public NegativeException(String message){
-//			super(message);
-//		}
-//	}
-
-
+	public static class NegativeException extends Exception{
+		public NegativeException(){
+			super();
+		}
+		public NegativeException(String message){
+			super(message);
+		}
+	}
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -2,52 +2,52 @@ package is.ru.stringcalculator;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-//import org.junit.rules.ExpectedException;
-//import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.junit.Rule;
 
 public class CalculatorTest {
 
-	public static void main(String args[]) //throws Calculator.NegativeException {
+	public static void main(String args[]) throws Calculator.NegativeException {
      		org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
    	}
 
 	@Test
-	public void testEmptyString() //throws Calculator.NegativeException {
+	public void testEmptyString() throws Calculator.NegativeException {
 		assertEquals(0, Calculator.add(""));
 	}
 
 	@Test
-	public void testOneNumber() //throws Calculator.NegativeException {
+	public void testOneNumber() throws Calculator.NegativeException {
 		assertEquals(1, Calculator.add("1"));
 	}
 
 	@Test
-	public void testTwoNumbers() //throws Calculator.NegativeException {
+	public void testTwoNumbers() throws Calculator.NegativeException {
 		assertEquals(3, Calculator.add("1,2"));
 	}
 
 	@Test
-	public void testMultipleNumbers() //throws Calculator.NegativeException {
+	public void testMultipleNumbers() throws Calculator.NegativeException {
     		assertEquals(6, Calculator.add("1,2,3"));
    	}
 
 	@Test
-	public void testNewLine() //throws Calculator.NegativeException {
+	public void testNewLine() throws Calculator.NegativeException {
 		assertEquals(6, Calculator.add("1\n2,3"));
 	}
 
 	@Test
-	public void testAllDelimiters() //throws Calculator.NegativeException {
+	public void testAllDelimiters() throws Calculator.NegativeException {
 		assertEquals(3, Calculator.add("//;\n1;2"));
 	}
 
-//	@Rule
-//	public ExpectedException thrown = (ExpectedException.none());
+	@Rule
+	public ExpectedException thrown = (ExpectedException.none());
 
-//	@Test
-//	public void testNegative() throws Calculator.NegativeException {
-//		thrown.expect(Calculator.NegativeException.class);
-//		thrown.expectMessage("Negatives not allowed: -4, -5");
-//		assertEquals(5, Calculator.add("2,-4,3,-5"));
-//	}
+	@Test
+	public void testNegative() throws Calculator.NegativeException {
+		thrown.expect(Calculator.NegativeException.class);
+		thrown.expectMessage("Negatives not allowed: -4,-5");
+		assertEquals(5, Calculator.add("2,-4,3,-5"));
+	}
 }
